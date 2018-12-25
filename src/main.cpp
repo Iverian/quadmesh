a8e3195119482636e853959cc5987ab8fd3a5ddd
Артем Старченко <41ways1ucky@gmail.com>
2018-12-26 02:03:38 +0300

    added tests

--- src/main.cpp
+++ src/main.cpp
@@ -14,24 +14,5 @@ using namespace std;
 
 int main(int argc, char const* argv[])
 {
-
-    if (argc > 1) {
-        fmt::print(cerr, "filename: {}", argv[1]);
-
-        auto in = ifstream(argv[1]);
-        auto manifold = step_parse(in);
-        cout << "[" << endl;
-        for (auto i = begin(manifold); i != end(manifold); ++i) {
-            auto& edges = i->edges();
-            for (auto j = begin(edges); j != end(edges); ++j) {
-                auto flag
-                    = (next(j) == end(edges) && next(i) == end(manifold));
-                auto d = j->curve().discretize(j->pfront(), j->pback());
-                cout << RangePrint(begin(d), end(d)) << (flag ? "\n" : ",");
-            }
-        }
-        cout << "]" << endl;
-    }
-
     return 0;
 }
--- test/src/test_surface_builder.cpp
+++ test/src/test_surface_builder.cpp
@@ -7,74 +7,102 @@
 #include <geom_model/shell.h>
 #include <geom_model/surfaces.h>
 #include <quadmesh/quadmesh_config.h>
+#include <step_parser.h>
 
-#include <cmath>
 #include <mesh/quad_mesh_builder.h>
 #include <util/debug.h>
 #include <util/itertools.h>
 #include <util/util.h>
 
+#include <cmath>
+
 using namespace std;
 
-struct Data {
-    Data()
-        : s()
-    {
-        array<Axis, 3> ax = {Axis::from_zx({0, 0, 1}, {1, 0, 0}, {0, 0, 0}),
-                             Axis::from_zx({0, 0, -1}, {0, -1, 0}, {0, 0, 0}),
-                             Axis::from_zx({-1, 0, 0}, {0, -1, 0}, {0, 0, 0})};
-        array<Edge, 3> e
-            = {Edge(make_shared<Circle>(1, ax[1]), M_PI, 2 * M_PI),
-               Edge(make_shared<Circle>(1, ax[2]), 0, M_PI),
-               Edge(make_shared<Line>(Vec(0, 1, 0), Point(0, 0, 0)), -1, 1)};
-        array<shared_ptr<AbstractSurface>, 3> g
-            = {make_shared<Plane>(ax[1]), make_shared<Plane>(ax[2]),
-               make_shared<SphericalSurface>(1, ax[0])};
-        array<Face, 3> f
-            = {Face(g[0], true,
-                    {OrientedEdge(e[0], true), OrientedEdge(e[2], true)}),
-               Face(g[1], true,
-                    {OrientedEdge(e[1], true), OrientedEdge(e[2], false)}),
-               Face(g[2], true,
-                    {OrientedEdge(e[0], false), OrientedEdge(e[1], false)})};
-        s = Shell(ax[0], {f[0], f[1], f[2]});
-    }
-
-    Shell s;
-};
-
-class TestSurfaceBuilder : public ::testing::Test {
-protected:
-    void SetUp()
-    {
-        data = make_unique<Data>();
-    }
-    void TearDown()
-    {
-        data.reset();
-    }
-    std::unique_ptr<Data> data;
-};
-
-TEST_F(TestSurfaceBuilder, test_1)
+TEST(TestSurfaceBuilder, test_cube)
 {
-    auto builder = QuadMeshBuilder(data->s, QuadmeshConfig());
-    auto mesh = builder.get().mesh();
-    cout << *mesh << endl;
+    auto manifold = step_parse("step/cube.stp");
+    auto& shell = manifold[0];
 
-    ofstream of("sphere_soup.json");
+    ofstream sf("out/cube_gm.json");
+    sf << shell;
+    sf.close();
+
+    auto builder = QuadMeshBuilder(shell, QuadmeshConfig());
+    auto mesh = builder.get().mesh();
+
     auto e = mesh->get_edge_soup();
     auto v = mesh->get_vertex_soup();
 
+    ofstream of("out/cube_es.json");
     fmt::print(of, "{{ \"vertices\": {}, \"edges\": {} }}",
+               RangePrint(begin(v), end(v)), RangePrint(begin(e), end(e)));
+    of.close();
+
+    SUCCEED();
+}
+
+TEST(TestSurfaceBuilder, test_parabcyl)
+{
+    auto manifold = step_parse("step/parabcyl.stp");
+    auto& shell = manifold[0];
+
+    ofstream sf("out/parabcyl_gm.json");
+    sf << shell;
+    sf.close();
+
+    auto builder = QuadMeshBuilder(shell, QuadmeshConfig());
+    auto mesh = builder.get().mesh();
+
+    auto e = mesh->get_edge_soup();
+    auto v = mesh->get_vertex_soup();
+
+    ofstream of("out/parabcyl_es.json");
+    fmt::print(of, "{{ \"vertices\": {}, \"edges\": {} }}",
+               RangePrint(begin(v), end(v)), RangePrint(begin(e), end(e)));
+    of.close();
+
+    SUCCEED();
+}
+
+TEST(TestSurfaceBuilder, test_spherecyl)
+{
+    auto manifold = step_parse("step/spherecyl.stp");
+    auto& shell = manifold[0];
+
+    ofstream("out/spherecyl_gm.json") << shell;
+
+    auto builder = QuadMeshBuilder(shell, QuadmeshConfig());
+    auto mesh = builder.get().mesh();
+    ofstream("out/spherecyl_ms.json") << *mesh;
+
+    auto e = mesh->get_edge_soup();
+    auto v = mesh->get_vertex_soup();
+    fmt::print(ofstream("out/spherecyl_es.json"),
+               "{{ \"vertices\": {}, \"edges\": {} }}",
                RangePrint(begin(v), end(v)), RangePrint(begin(e), end(e)));
 
     SUCCEED();
 }
 
-TEST_F(TestSurfaceBuilder, test_2)
+TEST(TestSurfaceBuilder, test_qcircle)
 {
-    auto f = data->s.faces()[0];
-    auto& s = f.surface();
-    DEBUG_FMT_("n : {}, t: {}", s.normal({0, 0}), s.tangent({1, 1}));
+    auto manifold = step_parse("step/qcircle.stp");
+    auto& shell = manifold[0];
+
+    ofstream sf("out/qcircle_gm.json");
+    sf << shell;
+    sf.close();
+
+    auto builder = QuadMeshBuilder(shell, QuadmeshConfig());
+    auto mesh = builder.get().mesh();
+
+    auto e = mesh->get_edge_soup();
+    auto v = mesh->get_vertex_soup();
+
+    ofstream of("out/qcircle_es.json");
+    fmt::print(of, "{{ \"vertices\": {}, \"edges\": {} }}",
+               RangePrint(begin(v), end(v)), RangePrint(begin(e), end(e)));
+    of.close();
+
+    SUCCEED();
 }
